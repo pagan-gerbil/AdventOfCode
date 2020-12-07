ed1065902b992a3c59c7d7a7b0d611f2bc9c6f8c
Pete Fullergreen <pete@pagan-gerbil.net>
2020-12-07 08:36:31 +0000

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = File.ReadAllLines("input.txt").Select(int.Parse);
+
+            var target = 2020;
+
+            foreach (var n in numbers)
+            {
+                var remainder = target - n;
+                if (numbers.Contains(remainder))
+                {
+                    Console.WriteLine($"Found numbers {n} + {remainder} = 2020");
+                    Console.WriteLine($"The answer is {n} * {remainder} = {n * remainder}");
+                }
+            }
+        }
+    }
+}
