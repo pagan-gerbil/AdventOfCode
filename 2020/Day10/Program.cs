da462bf22c5344562f3926bfea107ba24169ebaa
Pete Fullergreen <pete@pagan-gerbil.net>
2020-12-10 11:08:56 +0000

    day 10 part 1 solved

--- 2020/Day10/Program.cs
+++ 2020/Day10/Program.cs
@@ -7,7 +7,7 @@ namespace Day10
     {
         static void Main(string[] args)
         {
-            var adapters = _longerExample
+            var adapters = _input
                 .Split(Environment.NewLine)
                 .Select(x => int.Parse(x));
             var device = adapters.Max() + 3;
@@ -37,7 +37,6 @@ namespace Day10
 
             Console.WriteLine($"{diff1} differences of 1 jolt; {diff3} differences of 3 jolt");
             Console.WriteLine($"Answer is {diff1 * diff3}");
-
         }
 
         private static string _example = @"16
@@ -82,5 +81,99 @@ namespace Day10
 34
 10
 3";
+        private static string _input = @"71
+30
+134
+33
+51
+115
+122
+38
+61
+103
+21
+12
+44
+129
+29
+89
+54
+83
+96
+91
+133
+102
+99
+52
+144
+82
+22
+68
+7
+15
+93
+125
+14
+92
+1
+146
+67
+132
+114
+59
+72
+107
+34
+119
+136
+60
+20
+53
+8
+46
+55
+26
+126
+77
+65
+78
+13
+108
+142
+27
+75
+110
+90
+35
+143
+86
+116
+79
+48
+113
+101
+2
+123
+58
+19
+76
+16
+66
+135
+64
+28
+9
+6
+100
+124
+47
+109
+23
+139
+145
+5
+45
+106
+41";
     }
 }
