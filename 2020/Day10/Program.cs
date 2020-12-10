8daa10a0617eb73b305f56300f880c7c1f495612
Pete Fullergreen <pete@pagan-gerbil.net>
2020-12-10 11:03:29 +0000

    day 10 part 1; promising example

--- 2020/Day10/Program.cs
+++ 2020/Day10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Day10
 {
@@ -6,7 +7,49 @@ namespace Day10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var adapters = _example
+                .Split(Environment.NewLine)
+                .Select(x => int.Parse(x));
+            var device = adapters.Max() + 3;
+            
+            adapters = adapters
+                .Concat(new[] { device })
+                .OrderBy(x => x);
+
+            var diff1 = 0;
+            var diff2 = 0;
+            var diff3 = 0;
+
+            var lastAdapter = 0;
+
+            foreach (var adapter in adapters)
+            {
+                if (adapter - lastAdapter == 1)
+                    diff1++;
+                if (adapter - lastAdapter == 2)
+                    diff2++;
+                if (adapter - lastAdapter == 3)
+                    diff3++;
+                if (adapter - lastAdapter > 3)
+                    Console.WriteLine("Something odd?");
+                lastAdapter = adapter;
+            }
+
+            Console.WriteLine($"{diff1} differences of 1 jolt; {diff3} differences of 3 jolt");
+            Console.WriteLine($"Answer is {diff1 * diff3}");
+
         }
+
+        private static string _example = @"16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4";
     }
 }
