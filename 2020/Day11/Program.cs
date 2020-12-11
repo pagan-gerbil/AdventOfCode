52a9b3d3c2f031fba750f4fcc25f6b1c8df26308
Pete Fullergreen <pete@pagan-gerbil.net>
2020-12-11 09:59:17 +0000

    example day 11 works

--- 2020/Day11/Program.cs
+++ 2020/Day11/Program.cs
@@ -12,29 +12,71 @@ namespace Day11
                 .Select(x => x.ToCharArray())
                 .ToArray();
 
-            var newSeatArray = new char[seatArray.Length, seatArray[0].Length];
-            seatArray.CopyTo(newSeatArray, 0);
+            var changed = true;
 
-            var changed = false;
-
-            for(var row = 0; row < seatArray.Length; row++)
+            while (changed == true)
             {
-                for (var col = 0; col < seatArray[row].Length; col++)
+                changed = false;
+                var newSeatArray = seatArray.Select(x => string.Join(null, x).ToCharArray()).ToArray();
+                
+                for (var row = 0; row < seatArray.Length; row++)
                 {
-                    switch(seatArray[row][col])
+                    for (var col = 0; col < seatArray[row].Length; col++)
                     {
-                        case '.':
-                            continue;
-                        case 'L':
-                            //check all adjacent
-                            break;
-                        case '#':
-                            //count till 4
-                            break;
+                        var count = CountAdjacent(4, seatArray, row, col);
+                        switch (seatArray[row][col])
+                        {
+                            case '.':
+                                continue;
+                            case 'L':
+                                if (count == 0)
+                                {
+                                    newSeatArray[row][col] = '#';
+                                    changed = true;
+                                }
+                                break;
+                            case '#':
+                                if (count >= 4)
+                                {
+                                    newSeatArray[row][col] = 'L';
+                                    changed = true;
+                                }
+                                break;
+                        }
                     }
+                }
+
+                seatArray = newSeatArray;
+            }
+
+            Console.WriteLine("Loop exited");
+            Console.WriteLine($"There are {seatArray.Sum(x => x.Count(y => y == '#'))} occupied seats");
+        }
+
+        private static int CountAdjacent(int cutoff, char[][] seatArray, int row, int col)
+        {
+            var rowStart = row == 0 ? 0 : -1;
+            var rowEnd = row == seatArray.Length - 1 ? 1 : 2;
+            var colStart = col == 0 ? 0 : -1;
+            var colEnd =   col == seatArray[0].Length - 1 ? 1 : 2;
+            var counter = 0;
+            
+            for (var rowMod = rowStart; rowMod < rowEnd; rowMod++)
+            {
+                for (var colMod = colStart; colMod < colEnd; colMod++)
+                {
+                    if (rowMod == 0 && colMod == 0)
+                        continue;
+
+                    if (seatArray[row + rowMod][col + colMod] == '#')
+                        counter++;
+
+                    if (counter == cutoff)
+                        return counter;
                 }
             }
 
+            return counter;
         }
 
         private static string _example = @"L.LL.LL.LL
