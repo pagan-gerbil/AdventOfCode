c38259b5650224b065a012f72e0dcd0d1808dc76
Pete Fullergreen <pete@pagan-gerbil.net>
2020-12-11 09:38:10 +0000

    early setup

--- 2020/Day11/Program.cs
+++ 2020/Day11/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Day11
 {
@@ -6,7 +7,45 @@ namespace Day11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var seatArray = _example
+                .Split(Environment.NewLine)
+                .Select(x => x.ToCharArray())
+                .ToArray();
+
+            var newSeatArray = new char[seatArray.Length, seatArray[0].Length];
+            seatArray.CopyTo(newSeatArray, 0);
+
+            var changed = false;
+
+            for(var row = 0; row < seatArray.Length; row++)
+            {
+                for (var col = 0; col < seatArray[row].Length; col++)
+                {
+                    switch(seatArray[row][col])
+                    {
+                        case '.':
+                            continue;
+                        case 'L':
+                            //check all adjacent
+                            break;
+                        case '#':
+                            //count till 4
+                            break;
+                    }
+                }
+            }
+
         }
+
+        private static string _example = @"L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL";
     }
 }
