6b0ba68f2a01883260fbbd444e1595e2dca56f89
Pete Fullergreen <pete@pagan-gerbil.net>
2020-12-12 09:07:25 +0000

    setting up day 12

---
+++ 2020/Day12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static string _example = @"F10
+N3
+F7
+R90
+F11";
+    }
+}
