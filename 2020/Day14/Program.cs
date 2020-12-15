6dabaec5fc67126846f1e927b387ccb493351bcc
Pete Fullergreen <pete@pagan-gerbil.net>
2020-12-15 11:13:14 +0000

    structure for day 14

--- 2020/Day14/Program.cs
+++ 2020/Day14/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day14
 {
@@ -6,7 +8,31 @@ namespace Day14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = _example.Split(Environment.NewLine);
+
+            var mask = lines[0].Substring(7);
+            var instructions = lines.Skip(1)
+                .Select(x =>
+                {
+                    var firstIndex = x.IndexOf('[');
+                    var lastIndex = x.LastIndexOf(']');
+
+                    return new Tuple<int, int>(int.Parse(x.Substring(firstIndex, lastIndex - firstIndex)), int.Parse(x.Substring(x.LastIndexOf(' '))));
+                }).Reverse();
+
+            var results = new Dictionary<int, int>();
+            
+            foreach (var instruction in instructions)
+            {
+                if (results.ContainsKey(instruction.Item1))
+                    continue;
+
+                int value = instruction.Item2;
+                // apply mask here....
+                results.Add(instruction.Item1, value);
+            }
+
+            Console.WriteLine($"The sum of masked values is {results.Values.Sum()}");
         }
 
         private static string _example = @"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
