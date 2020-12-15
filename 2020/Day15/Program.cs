53faa3a924cebe2295d9a39c53e27c0bbd534ae7
Pete Fullergreen <pete@pagan-gerbil.net>
2020-12-15 08:18:05 +0000

    day 15 part 1 complete

--- 2020/Day15/Program.cs
+++ 2020/Day15/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Day15
 {
@@ -6,7 +7,29 @@ namespace Day15
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = _input.Split(',').Select(x => int.Parse(x)).ToList();
+
+            var counter = numbers.Count();
+            while (counter < 2020)
+            {
+                var lastNumber = numbers.Last();
+                if (numbers.Count(x => x == lastNumber) > 1)
+                {
+                    numbers.Add( numbers.LastIndexOf(lastNumber) - numbers.Take(numbers.LastIndexOf(lastNumber)).ToList().LastIndexOf(lastNumber));
+                }
+                else if (numbers.Count(x => x == lastNumber) == 1)
+                {
+                    numbers.Add(0);
+                }
+                
+                counter++;
+            }
+
+            Console.WriteLine($"The 2020th number is {numbers.Last()}");
+
         }
+
+        private static string _example = @"0,3,6";
+        private static string _input = @"11,18,0,20,1,7,16";
     }
 }
