1809bbeb39d86158781685a02d388c38de4cd97d
Pete Fullergreen <pete@pagan-gerbil.net>
2020-12-15 11:05:09 +0000

    setting up day 14 out of sequence

---
+++ 2020/Day14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static string _example = @"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0";
+    }
+}
