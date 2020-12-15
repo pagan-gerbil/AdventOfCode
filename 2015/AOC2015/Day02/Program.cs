6a1ec6999b0f2047c49b548070ab4eacc450a190
Pete Fullergreen <pete@pagan-gerbil.net>
2020-12-15 16:09:48 +0000

    2015 day 2 example working

--- 2015/AOC2015/Day02/Program.cs
+++ 2015/AOC2015/Day02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Day02
 {
@@ -6,7 +7,27 @@ namespace Day02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var total = 0l;
+
+            foreach (var line in _example.Split(Environment.NewLine))
+            {
+                var numbers = line.Split('x').Select(long.Parse).ToArray();
+
+                var sides = new[]
+                {
+                    numbers[0] * numbers[1],
+                    numbers[2] * numbers[1],
+                    numbers[0] * numbers[2]
+                };
+
+                total += sides.Min() + (2 * sides.Sum());
+                
+            }
+
+            Console.WriteLine($"Total amount of paper is: {total}");
         }
+
+        private static string _example = @"2x3x4
+1x1x10";
     }
 }
