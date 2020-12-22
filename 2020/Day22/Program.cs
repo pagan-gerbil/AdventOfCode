996ba072cd5d4d53ad57611e4e7a1f812da293a0
Pete Fullergreen <pete@pagan-gerbil.net>
2020-12-22 11:00:01 +0000

    day 22 part 1 complete

--- 2020/Day22/Program.cs
+++ 2020/Day22/Program.cs
@@ -10,9 +10,9 @@ namespace Day22
             var player1 = new Queue<int>();
             var player2 = new Queue<int>();
 
-            foreach (var line in _player1Example.Split(Environment.NewLine))
+            foreach (var line in _player1Input.Split(Environment.NewLine))
                 player1.Enqueue(int.Parse(line));
-            foreach (var line in _player2Example.Split(Environment.NewLine))
+            foreach (var line in _player2Input.Split(Environment.NewLine))
                 player2.Enqueue(int.Parse(line));
 
             while (player1.Count > 0 && player2.Count > 0)
@@ -54,5 +54,57 @@ namespace Day22
 4
 7
 10";
+
+        private static string _player1Input = @"31
+24
+5
+33
+7
+12
+30
+22
+48
+14
+16
+26
+18
+45
+4
+42
+25
+20
+46
+21
+40
+38
+34
+17
+50";
+
+        private static string _player2Input = @"1
+3
+41
+8
+37
+35
+28
+39
+43
+29
+10
+27
+11
+36
+49
+32
+2
+23
+19
+9
+13
+15
+47
+6
+44";
     }
 }
