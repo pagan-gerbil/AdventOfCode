f13e1a86f025e3ededdf90ae4d500deba2cf4c3c
Pete Fullergreen <pete@pagan-gerbil.net>
2020-12-22 10:49:45 +0000

    day 22 example part 1 working

--- 2020/Day22/Program.cs
+++ 2020/Day22/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day22
 {
@@ -6,7 +7,52 @@ namespace Day22
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var player1 = new Queue<int>();
+            var player2 = new Queue<int>();
+
+            foreach (var line in _player1Example.Split(Environment.NewLine))
+                player1.Enqueue(int.Parse(line));
+            foreach (var line in _player2Example.Split(Environment.NewLine))
+                player2.Enqueue(int.Parse(line));
+
+            while (player1.Count > 0 && player2.Count > 0)
+            {
+                var p1Play = player1.Dequeue();
+                var p2Play = player2.Dequeue();
+
+                if (p1Play > p2Play)
+                {
+                    player1.Enqueue(p1Play);
+                    player1.Enqueue(p2Play);
+                }
+                else
+                {
+                    player2.Enqueue(p2Play);
+                    player2.Enqueue(p1Play);
+                }
+            }
+
+            var score = 0;
+
+            while (player1.Count > 0 || player2.Count > 0)
+            {
+                if (player1.Count > 0) score += (player1.Count * player1.Dequeue());
+                if (player2.Count > 0) score += (player2.Count * player2.Dequeue());
+            }
+
+            Console.WriteLine($"The winning score is {score}");
         }
+
+        private static string _player1Example = @"9
+2
+6
+3
+1";
+
+        private static string _player2Example = @"5
+8
+4
+7
+10";
     }
 }
