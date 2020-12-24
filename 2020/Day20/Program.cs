e64ed49208ba9ca5b53632ddb06df37e0cd953bb
Pete Fullergreen <pete@pagan-gerbil.net>
2020-12-24 18:10:28 +0000

    reading tiles

--- 2020/Day20/Program.cs
+++ 2020/Day20/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day20
 {
@@ -6,7 +8,152 @@ namespace Day20
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var currentTile = new Tile();
+            var allTiles = new List<Tile>();
+
+            foreach (var line in _example.Split(Environment.NewLine))
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    continue;
+                }
+
+                if (line.StartsWith("Tile"))
+                {
+                    if (currentTile.Id > 0) allTiles.Add(currentTile);
+                    currentTile = new Tile { Id = int.Parse(line.Substring(4, line.Length-5)) };
+                    continue;
+                }
+
+                if (string.IsNullOrEmpty(currentTile.Top))
+                {
+                    currentTile.Top = line;
+                }
+
+                currentTile.Left += line[0];
+                currentTile.Right += line.Last();
+
+                if (currentTile.Right.Length.Equals(currentTile.Top.Length))
+                    currentTile.Bottom = line;
+            }
         }
+
+        public class Tile
+        {
+            public int Id { get; set; }
+            public string Top { get; set; }
+            public string Bottom { get; set; }
+            public string Left { get; set; } = string.Empty;
+            public string Right { get; set; } = string.Empty;
+        }
+
+
+        private static string _example = @"Tile 2311:
+..##.#..#.
+##..#.....
+#...##..#.
+####.#...#
+##.##.###.
+##...#.###
+.#.#.#..##
+..#....#..
+###...#.#.
+..###..###
+
+Tile 1951:
+#.##...##.
+#.####...#
+.....#..##
+#...######
+.##.#....#
+.###.#####
+###.##.##.
+.###....#.
+..#.#..#.#
+#...##.#..
+
+Tile 1171:
+####...##.
+#..##.#..#
+##.#..#.#.
+.###.####.
+..###.####
+.##....##.
+.#...####.
+#.##.####.
+####..#...
+.....##...
+
+Tile 1427:
+###.##.#..
+.#..#.##..
+.#.##.#..#
+#.#.#.##.#
+....#...##
+...##..##.
+...#.#####
+.#.####.#.
+..#..###.#
+..##.#..#.
+
+Tile 1489:
+##.#.#....
+..##...#..
+.##..##...
+..#...#...
+#####...#.
+#..#.#.#.#
+...#.#.#..
+##.#...##.
+..##.##.##
+###.##.#..
+
+Tile 2473:
+#....####.
+#..#.##...
+#.##..#...
+######.#.#
+.#...#.#.#
+.#########
+.###.#..#.
+########.#
+##...##.#.
+..###.#.#.
+
+Tile 2971:
+..#.#....#
+#...###...
+#.#.###...
+##.##..#..
+.#####..##
+.#..####.#
+#..#.#..#.
+..####.###
+..#.#.###.
+...#.#.#.#
+
+Tile 2729:
+...#.#.#.#
+####.#....
+..#.#.....
+....#..#.#
+.##..##.#.
+.#.####...
+####.#.#..
+##.####...
+##..#.##..
+#.##...##.
+
+Tile 3079:
+#.#.#####.
+.#..######
+..#.......
+######....
+####.#..#.
+.#...#.##.
+#.#####.##
+..#.###...
+..#.......
+..#.###...";
     }
 }
