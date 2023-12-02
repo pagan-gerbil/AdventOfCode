95b7ff4b934a4ca4e080199b13f2bae43fa47fc4
Pete Fullergreen <pete@pagan-gerbil.net>
2023-12-02 20:58:07 +0000

    2022 Day 16, incomplete :(

--- 2022/AdventOfCode2022/AdventOfCode2022/Day16.cs
+++ 2022/AdventOfCode2022/AdventOfCode2022/Day16.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2022
 {
@@ -14,16 +14,215 @@ namespace AdventOfCode2022
             Puzzle2();
         }
 
+        private static Regex _inputRegex = new Regex("Valve (?<valve>[A-Z]+) has flow rate=(?<flowRate>[0-9]+); tunnels lead to valves (?<outs>[A-Z, ]+)");
+
         private static void Puzzle1()
         {
+            var nodes = new List<Node>();
+            foreach (var line in _testInput.Split(Environment.NewLine))
+            {
+                var match = _inputRegex.Match(line);
+                var valve = match.Groups["valve"].Value;
+                var flowRate = int.Parse(match.Groups["flowRate"].Value);
+                var outs = match.Groups["outs"].Value.Split(',').Select(x => x.Trim());
+
+                nodes.Add(new Node { Valve = valve, FlowRate = flowRate, Outs = outs });
+            }
+
+            var edges = nodes.SelectMany(x => x.Outs.Select(y => new Tuple<Node, Node>(x, nodes.Single(z => y == z.Valve))));
+            var graph = new Graph<Node>(nodes, edges);
+
+            var algorithms = new Algorithms();
         }
 
         private static void Puzzle2()
         {
         }
 
-        private static string _testInput = @"";
+        public class Node
+        {
+            public string Valve { get; set; }
+            public int FlowRate { get; set; }
+            public IEnumerable<string> Outs { get; set; }
+        }
 
-        private static string _input = @"";
+        public class Graph<T> where T : notnull
+        {
+            public Graph() { }
+            public Graph(IEnumerable<T> vertices, IEnumerable<Tuple<T, T>> edges)
+            {
+                foreach (var vertex in vertices) AddVertex(vertex);
+
+                foreach (var edge in edges) AddEdge(edge);
+            }
+
+            public Dictionary<T, HashSet<T>> AdjacencyList { get; } = new Dictionary<T, HashSet<T>>();
+
+            public void AddVertex(T vertex)
+            {
+                AdjacencyList[vertex] = new HashSet<T>();
+            }
+
+            public void AddEdge(Tuple<T, T> edge)
+            {
+                if (AdjacencyList.ContainsKey(edge.Item1) && AdjacencyList.ContainsKey(edge.Item2))
+                {
+                    AdjacencyList[edge.Item1].Add(edge.Item2);
+                    AdjacencyList[edge.Item2].Add(edge.Item1);
+                }
+            }
+        }
+
+        public class Algorithms
+        {
+            public HashSet<T> BFS<T>(Graph<T> graph, T start, Action<T> preVisit = null) where T : notnull
+            {
+                var visited = new HashSet<T>();
+
+                if (!graph.AdjacencyList.ContainsKey(start))
+                    return visited;
+
+                var queue = new Queue<T>();
+                queue.Enqueue(start);
+
+                while (queue.Count > 0)
+                {
+                    var vertex = queue.Dequeue();
+
+                    if (visited.Contains(vertex))
+                        continue;
+
+                    if (preVisit != null)
+                        preVisit(vertex);
+
+                    visited.Add(vertex);
+
+                    foreach (var neighbor in graph.AdjacencyList[vertex])
+                        if (!visited.Contains(neighbor))
+                            queue.Enqueue(neighbor);
+                }
+
+                return visited;
+            }
+
+            public Func<T, IEnumerable<T>> ShortestPathFunction<T>(Graph<T> graph, T start, Func<T, T, bool> isValidFunc = null) where T : notnull
+            {
+                var previous = new Dictionary<T, T>();
+
+                var queue = new Queue<T>();
+                queue.Enqueue(start);
+
+                while (queue.Count > 0)
+                {
+                    var vertex = queue.Dequeue();
+                    foreach (var neighbor in graph.AdjacencyList[vertex])
+                    {
+                        if (previous.ContainsKey(neighbor))
+                            continue;
+
+                        if (isValidFunc == null || isValidFunc(vertex, neighbor))
+                        {
+                            previous[neighbor] = vertex;
+                            queue.Enqueue(neighbor);
+                        }
+                    }
+                }
+
+                Func<T, IEnumerable<T>> shortestPath = v => {
+                    if (!previous.ContainsKey(v)) return null;
+
+                    var path = new List<T> { };
+
+                    var current = v;
+                    while (!current.Equals(start))
+                    {
+                        path.Add(current);
+                        current = previous[current];
+                    };
+
+                    path.Add(start);
+                    path.Reverse();
+
+                    return path;
+                };
+
+                return shortestPath;
+            }
+        }
+
+        private static string _testInput = @"Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II";
+
+        private static string _input = @"Valve OQ has flow rate=17; tunnels lead to valves NB, AK, KL
+Valve HP has flow rate=0; tunnels lead to valves ZX, KQ
+Valve GO has flow rate=0; tunnels lead to valves HR, GW
+Valve PD has flow rate=9; tunnels lead to valves XN, EV, QE, MW
+Valve NQ has flow rate=0; tunnels lead to valves HX, ZX
+Valve DW has flow rate=0; tunnels lead to valves IR, WE
+Valve TN has flow rate=24; tunnels lead to valves KL, EI
+Valve JJ has flow rate=0; tunnels lead to valves EV, HR
+Valve KH has flow rate=0; tunnels lead to valves ZQ, AA
+Valve PH has flow rate=0; tunnels lead to valves FN, QE
+Valve FD has flow rate=0; tunnels lead to valves SM, HX
+Valve SM has flow rate=7; tunnels lead to valves WW, RZ, FD, HO, KQ
+Valve PU has flow rate=0; tunnels lead to valves VL, IR
+Valve OM has flow rate=0; tunnels lead to valves CM, AA
+Valve KX has flow rate=20; tunnel leads to valve PC
+Valve IR has flow rate=3; tunnels lead to valves PU, CM, WW, DW, AF
+Valve XG has flow rate=0; tunnels lead to valves RX, OF
+Valve QE has flow rate=0; tunnels lead to valves PH, PD
+Valve GW has flow rate=0; tunnels lead to valves JQ, GO
+Valve HO has flow rate=0; tunnels lead to valves SM, TY
+Valve WU has flow rate=0; tunnels lead to valves SG, RZ
+Valve MS has flow rate=0; tunnels lead to valves UE, OF
+Valve JS has flow rate=0; tunnels lead to valves DO, ZX
+Valve YQ has flow rate=0; tunnels lead to valves BC, SG
+Valve EJ has flow rate=0; tunnels lead to valves AA, LR
+Valve EI has flow rate=0; tunnels lead to valves BV, TN
+Valve NC has flow rate=0; tunnels lead to valves TS, BC
+Valve AF has flow rate=0; tunnels lead to valves IR, HX
+Valve OX has flow rate=0; tunnels lead to valves HR, BV
+Valve BF has flow rate=0; tunnels lead to valves JQ, SY
+Valve CA has flow rate=0; tunnels lead to valves YD, HX
+Valve KQ has flow rate=0; tunnels lead to valves HP, SM
+Valve NB has flow rate=0; tunnels lead to valves OQ, OF
+Valve SY has flow rate=0; tunnels lead to valves BF, BV
+Valve AA has flow rate=0; tunnels lead to valves KH, EJ, OM, TY, DO
+Valve BC has flow rate=11; tunnels lead to valves WE, RX, YQ, LR, NC
+Valve HR has flow rate=14; tunnels lead to valves OX, GO, JJ
+Valve WE has flow rate=0; tunnels lead to valves DW, BC
+Valve MW has flow rate=0; tunnels lead to valves JQ, PD
+Valve DO has flow rate=0; tunnels lead to valves JS, AA
+Valve PC has flow rate=0; tunnels lead to valves AK, KX
+Valve YD has flow rate=0; tunnels lead to valves CA, OF
+Valve RX has flow rate=0; tunnels lead to valves XG, BC
+Valve CM has flow rate=0; tunnels lead to valves IR, OM
+Valve HX has flow rate=6; tunnels lead to valves ZQ, NQ, AF, FD, CA
+Valve ZQ has flow rate=0; tunnels lead to valves KH, HX
+Valve BV has flow rate=21; tunnels lead to valves SY, OX, EI
+Valve AK has flow rate=0; tunnels lead to valves PC, OQ
+Valve UE has flow rate=0; tunnels lead to valves MS, JQ
+Valve LR has flow rate=0; tunnels lead to valves BC, EJ
+Valve JQ has flow rate=8; tunnels lead to valves MW, UE, BF, GW
+Valve VL has flow rate=0; tunnels lead to valves PU, ZX
+Valve EV has flow rate=0; tunnels lead to valves JJ, PD
+Valve TS has flow rate=0; tunnels lead to valves NC, ZX
+Valve RZ has flow rate=0; tunnels lead to valves SM, WU
+Valve OF has flow rate=13; tunnels lead to valves XG, YD, NB, MS, XN
+Valve WW has flow rate=0; tunnels lead to valves SM, IR
+Valve TY has flow rate=0; tunnels lead to valves HO, AA
+Valve XN has flow rate=0; tunnels lead to valves OF, PD
+Valve SG has flow rate=15; tunnels lead to valves WU, YQ
+Valve FN has flow rate=25; tunnel leads to valve PH
+Valve KL has flow rate=0; tunnels lead to valves TN, OQ
+Valve ZX has flow rate=5; tunnels lead to valves JS, HP, VL, NQ, TS";
     }
 }
