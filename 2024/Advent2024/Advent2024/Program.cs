24b30d16dcc0f6c937b4db8fa57071e23d6c16b1
Pete Fullergreen <pete@pagan-gerbil.net>
2024-12-03 08:25:50 +0000

    2024 day 3 part 1 complete

--- 2024/Advent2024/Advent2024/Program.cs
+++ 2024/Advent2024/Advent2024/Program.cs
@@ -1,8 +1,4 @@
 ﻿using Advent2024;
 
-var puzzle = new Day02();
-puzzle.RunPart1(true);
-puzzle.RunPart1(false);
-
-puzzle.RunPart2(true);
-puzzle.RunPart2(false);
+var puzzle = new Day03();
+puzzle.Run();
---
+++ 2024/Advent2024/AdventUtils/DayBase.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Reflection.Emit;
+
+namespace AdventUtils;
+
+public abstract class DayBase
+{
+    protected abstract string _sample { get; }
+    protected abstract string _part1 { get; }
+    protected abstract string _part2 { get; }
+
+    public void Run()
+    {
+        Part1(_sample, true);
+        Part1(_part1, false);
+        Part2(_sample, true);
+        Part2(_part2, false);
+    }
+
+    private void Part1(string input, bool isTest)
+    {
+        var stopwatch = Stopwatch.StartNew();
+        var answer = Part1Internal(input);
+        stopwatch.Stop();
+
+        PrintAnswer(1, isTest, answer, stopwatch.ElapsedMilliseconds);
+    }
+
+    private void Part2(string input, bool isTest)
+    {
+        var stopwatch = Stopwatch.StartNew();
+        var answer = Part1Internal(input);
+        stopwatch.Stop();
+
+        PrintAnswer(2, isTest, answer, stopwatch.ElapsedMilliseconds);
+    }
+
+    private void PrintAnswer(int partNumber, bool isTest, string answer, long elapsedMilliseconds)
+    {
+        var label = isTest ? "test" : "final";
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write($"The {label} answer for part {partNumber} is ");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(answer);
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        Console.WriteLine($" - duration {elapsedMilliseconds}ms");
+    }
+
+    protected abstract string Part1Internal(string input);
+    protected abstract string Part2Internal(string input);
+}
